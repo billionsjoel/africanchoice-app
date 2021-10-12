cc91cbbe8ae806de869bf195a1f26fbd04bb8b61
Billions Joel <joelatugonza@gmail.com>
2021-10-12 12:08:10 +0300

    first commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600%7cPlayfair+Display:400i" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+
+    @yield('assets')
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+
+    </style>
+
+</head>
+
+<body class="body">
+    {{-- <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        @if (Route::has('login'))
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}"
+                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
+    </div> --}}
+
+    @include('layouts.nav')
+    <main>
+        @yield('content')
+    </main>
+    @include('layouts.footer')
+
+
+
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,35 @@
+ <nav class="navbar navbar-expand-md navbar-dark bg-dark p-4" aria-label="Fourth navbar example">
+     <div class="container-fluid container">
+         <a class="navbar-brand" href="#">African Choice Holdings</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04"
+             aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+
+         <div class="collapse navbar-collapse" id="navbarsExample04">
+             <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                 <li class="nav-item">
+                     <a class="nav-link active" aria-current="page" href="#">Home</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#">About us</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#">Products</a>
+                 </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#">Contact Us</a>
+                 </li>
+                 {{-- <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown"
+                         aria-expanded="false">Dropdown</a>
+                     <ul class="dropdown-menu" aria-labelledby="dropdown04">
+                         <li><a class="dropdown-item" href="#">Action</a></li>
+                         <li><a class="dropdown-item" href="#">Another action</a></li>
+                         <li><a class="dropdown-item" href="#">Something else here</a></li>
+                     </ul>
+                 </li> --}}
+             </ul>
+         </div>
+     </div>
+ </nav>
