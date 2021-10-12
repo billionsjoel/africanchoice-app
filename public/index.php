6217d7c608f2e341ace05eccd18f5150f4614b11
Billions Joel <joelatugonza@gmail.com>
2021-10-12 20:19:39 +0300

    final build -v

--- public/index.php
+++ public/index.php
@@ -46,6 +46,12 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// set the public path to this directory
+$app->bind('path.public', function () {
+    return __DIR__;
+});
+
+
 $kernel = $app->make(Kernel::class);
 
 $response = tap($kernel->handle(
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>African Choice Holdings ltd</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
